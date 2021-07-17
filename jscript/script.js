a5418faaf6eebb71728511363294d8866ae66595
Sujan <Sujankharel80@yahoo.com>
2021-07-17 13:57:33 -0700

    initial commit

---
+++ jscript/script.js
@@ -0,0 +1,9 @@
+function openSlideMenu() {
+    document.getElementById('menu').style.width = '200px';
+    document.getElementById('container').style.marginLeft = '200px';
+}
+
+function closeSlideMenu() {
+    document.getElementById('menu').style.width = '0';
+    document.getElementById('container').style.marginLeft = '35px';
+}
